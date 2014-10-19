57e68555bb9236c54f2e9e0bae553d9b4bfb3094
Gabriel Henrique Rudey <g_rudey@yahoo.com.br>
2014-10-19 16:13:18 -0200

    Organização de declarações e finalização dos interpretadores de operações aritméticas.

--- fonte/blank/lang/BlankInterpreter.java
+++ fonte/blank/lang/BlankInterpreter.java
@@ -26,31 +26,42 @@ public class BlankInterpreter
 		// Set auxiliar variable to temporaly store variables
 		BlankVar var = null;
 
-		// Match the "var" token
-		Pattern varIdentifier = Pattern.compile("\\bvar\\W+");
-		Matcher varMatcher    = varIdentifier.matcher(line);
+		Pattern commentIdentifier = Pattern.compile("\\/\\/[\\s\\S]+");
+		Matcher commentMatcher    = commentIdentifier.matcher(line);
 
-		// Match any word
-		Pattern paramIdentifier = Pattern.compile("\\w+");
-		Matcher paramMatcher    = paramIdentifier.matcher(line);
+		// Remove todos os comentários
+		if (commentMatcher.find()) {
+			String commentSection = commentMatcher.toMatchResult().group();
+			// Substitui na linha a expressão pelo resultado
+			line = line.replace(commentSection, "");
+		}
 
-		// Match a "word=word"
-		Pattern attributionIdentifier = Pattern.compile("\\w+(\\s+|\\b)\\=(\\s+|\\b)\\w+");
-		Matcher attributionMatcher    = attributionIdentifier.matcher(line);
+		/**
+		 *	Identifiers
+		 *	São os RegEx identificadores de cada parametro da linguagem
+		 */
+		Pattern varIdentifier         = Pattern.compile("\\bvar\\W+"); // Identifica "var"
+		Pattern paramIdentifier       = Pattern.compile("(\\w+|\\.+)+"); // Identifica um parametro
+		Pattern attributionIdentifier = Pattern.compile("\\w+(\\s+|\\b)\\=(\\s+|\\b)\\w+"); // Identifica "="
+		Pattern showIdentifier        = Pattern.compile("\\bshow\\W+"); // Identifica "show"
+		Pattern sumSubIdentifier      = Pattern.compile("\\w+(\\s+|\\b)(\\+|\\-)(\\s+|\\b)\\w+"); // Identifica "+" ou "-"
+		Pattern divMultIdentifier     = Pattern.compile("\\w+(\\s+|\\b)(\\*|\\/|\\%)(\\s+|\\b)\\w+"); // Identifica "*", "/" ou "%"
+		Pattern operationIdentifier   = Pattern.compile("(\\s+|\\b)(\\*|\\/|\\%|\\+|\\-|\\<|\\>)(\\s+|\\b)"); // Identifica uma operação (+, -, /, *, %, >, <, >=, ==, ...)
+		
+		/**
+		 *	Matchers
+		 *	Declaração dos Matchers, serão eles que irão, de fato, identificar o se a string contem
+		 *	o regex
+		 */
+		Matcher varMatcher;
+		Matcher paramMatcher;
+		Matcher attributionMatcher;
+		Matcher showMatcher;
+		Matcher sumSubMatcher;
+		Matcher divMultMatcher;
+		Matcher operationMatcher;
 
-		// Match "show"
-		Pattern showIdentifier = Pattern.compile("\\bshow\\W+");
-		Matcher showMatcher    = showIdentifier.matcher(line);
-
-		// Match + and - expressions
-		Pattern sumSubIdentifier = Pattern.compile("\\w+(\\s+|\\b)(\\+|\\-)(\\s+|\\b)\\w+");
-		Matcher sumSubMatcher    = sumSubIdentifier.matcher(line);
-
-		// Match + and - expressions
-		Pattern divMultIdentifier = Pattern.compile("\\w+(\\s+|\\b)(\\*|\\/|\\%)(\\s+|\\b)\\w+");
-		Matcher divMultMatcher    = divMultIdentifier.matcher(line);
-
-		// Find "var"
+		varMatcher = varIdentifier.matcher(line); // Prepara a verificação se existe o token var
 		if (varMatcher.find()) {
 			String analysis = line.split(varIdentifier.toString(), 2)[1]; // subject is after the var token
 			paramMatcher = paramIdentifier.matcher(analysis);
@@ -61,14 +72,16 @@ public class BlankInterpreter
 			}
 		}
 
-		// Encontra operações de *, / e %
+		divMultMatcher = divMultIdentifier.matcher(line); // prepara a busca para operações de *, / e %
 		if (divMultMatcher.find()) {
 			// Guarda a expressão encontrada para ser substituída mais tarde
 			String rawExpression = divMultMatcher.toMatchResult().group();
-			String[] analysis = line.split("(\\b|\\s+)(\\*|\\/|\\%)(\\b|\\s+)");
+			String[] analysis = rawExpression.split(operationIdentifier.toString());
 
 			// Identifica a operação realizada
-			String op   = (divMultIdentifier.matcher(line).toMatchResult().group());
+			operationMatcher = operationIdentifier.matcher(rawExpression);
+			operationMatcher.find();
+			String op   = operationMatcher.toMatchResult().group().trim();
 			String val1 = analysis[0].trim();
 			String val2 = analysis[1].trim();
 
@@ -105,7 +118,53 @@ public class BlankInterpreter
 			line = line.replace(rawExpression, exp.result().toString());
 		}
 
-		// Encontra "="
+		sumSubMatcher = sumSubIdentifier.matcher(line); // Prepara a busca para + e -
+		if (sumSubMatcher.find()) {
+			// Guarda a expressão encontrada para ser substituída mais tarde
+			String rawExpression = sumSubMatcher.toMatchResult().group();
+			String[] analysis = rawExpression.split(operationIdentifier.toString());
+
+			// Identifica a operação realizada
+			operationMatcher = operationIdentifier.matcher(rawExpression);
+			operationMatcher.find();
+			String op   = operationMatcher.toMatchResult().group().trim();
+			String val1 = analysis[0].trim();
+			String val2 = analysis[1].trim();
+
+			// Pega o primeiro valor
+			paramMatcher = paramIdentifier.matcher(val1);
+			if (paramMatcher.find()) {
+				String opValue1 = paramMatcher.toMatchResult().group();
+
+				if (mainScope.hasVariable(opValue1) >= 0) { // verifica se é uma variavel
+					var = mainScope.getVariable(opValue1);
+					val1 = var.getValue();
+				} else {
+					val1 = opValue1;
+				}
+			}
+
+			// Encontra o segundo valor
+			paramMatcher = paramIdentifier.matcher(val2);
+			if (paramMatcher.find()) {
+				String opValue2 = paramMatcher.toMatchResult().group();
+
+				if (mainScope.hasVariable(opValue2) >= 0) { // verifica se é uma variavel
+					var = mainScope.getVariable(opValue2);
+					val2 = var.getValue();
+				} else {
+					val2 = opValue2;
+				}
+			}
+
+			// Cria um novo calculo de expressão
+			BlankExpression exp = new BlankExpression(val1, val2, op);
+
+			// Substitui na linha a expressão pelo resultado
+			line = line.replace(rawExpression, exp.result().toString());
+		}
+
+		attributionMatcher = attributionIdentifier.matcher(line); // Prepara a busca para token "="
 		if (attributionMatcher.find()) {
 			String[] analysis = line.split("\\=");
 			String item  = analysis[0].trim();
@@ -137,7 +196,7 @@ public class BlankInterpreter
 			}
 		}
 
-		// Encontra a keyword "show"
+		showMatcher = showIdentifier.matcher(line); // Prepara a busca para o token show
 		if (showMatcher.find()) {
 			String analysis = line.split(showIdentifier.toString(), 2)[1]; // pega o que vem após "show"
 			paramMatcher    = paramIdentifier.matcher(analysis);
