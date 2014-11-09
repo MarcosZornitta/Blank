e4081599ad7a8780f9c5829e419cb551049f4349
Gabriel Henrique Rudey <g_rudey@yahoo.com.br>
2014-11-09 14:10:56 -0200

    Fix typos e adição de métodos para pegar informações isoladas passadas para classe através do construtor.

--- fonte/blank/lang/BlankException.java
+++ fonte/blank/lang/BlankException.java
@@ -1,5 +1,5 @@
 /**
- *	Extendendo a classe Exception do java para melhor mostrar
+ *	Estendendo a classe Exception do java para melhor mostrar
  *	um erro ocorrido dentro da Blank.
  *	Por enquanto apenas mostra uma mensagem melhor formatada.
  */
@@ -8,8 +8,31 @@ package blank.lang;
 
 class BlankException extends Exception
 {
+	private int errorLineNumber;
+	private String errorLine;
+	private String errorMessage;
+
 	public BlankException(String msg, int lineNumber, String line)
 	{
 		super(msg + " on line [" + (lineNumber + 1) + "]: " + line);
+
+		this.errorMessage    = msg;
+		this.errorLine       = line;
+		this.errorLineNumber = lineNumber;
+	}
+
+	public int getErrorLineNumber()
+	{
+		return this.errorLineNumber;
+	}
+
+	public String getErrorLine()
+	{
+		return this.errorLine;
+	}
+
+	public String getErrorMessage()
+	{
+		return this.errorMessage;
 	}
 }
