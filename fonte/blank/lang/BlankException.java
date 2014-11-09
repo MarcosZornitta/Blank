580b9e506b528f7283a024eec7ec47f4765b2c98
Gabriel Henrique Rudey <g_rudey@yahoo.com.br>
2014-11-09 12:04:22 -0200

    Adicionando classe customizada de Exceptions para melhor formatar as mensagens

---
+++ fonte/blank/lang/BlankException.java
@@ -0,0 +1,15 @@
+/**
+ *	Extendendo a classe Exception do java para melhor mostrar
+ *	um erro ocorrido dentro da Blank.
+ *	Por enquanto apenas mostra uma mensagem melhor formatada.
+ */
+
+package blank.lang;
+
+class BlankException extends Exception
+{
+	public BlankException(String msg, int lineNumber, String line)
+	{
+		super(msg + " on line [" + lineNumber + "]: " + line);
+	}
+}
